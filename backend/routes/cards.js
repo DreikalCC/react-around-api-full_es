4d98ae49ec86485a1899fab9e2160dfa1d9977c8
Aldo Villegas <avgaccs@gmail.com>
2023-05-16 15:34:19 -0700

    added cardRouter validators

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -10,24 +10,45 @@ const {
   dislikeCard,
 } = require("../controllers/cardsController");
 const { celebrate, Joi} = require('celebrate');
+const validateURL = (value, helpers) => {
+  if (validator.isURL(value)) {
+    return value;
+  }
+  return helpers.error('string.uri');
+}
 
 router.get("/", getCards);
 
 router.post("/", celebrate({
   body: Joi.object().keys({
     title:Joi.string().required().min(2).max(30),
-    link:Joi.string().required().min(7)
+    link:Joi.string().required().custom(validateURL),
   })
+  .unknown(true),
 }), postCard);
 
 router.delete("/:cardId", celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().alphanum()
-  })
+    cardId: Joi.string().alphanum(),
+  }),
+  headers: Joi.object().keys({}),
+  query: Joi.object().keys({}),
 }), deleteCard);
 
-router.put("/:cardId/likes", likeCard);
+router.put("/:cardId/likes", celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum(),
+  }),
+  headers: Joi.object().keys({}),
+  query: Joi.object().keys({}),
+}), likeCard);
 
-router.delete("/:cardId/likes", dislikeCard);
+router.delete("/:cardId/likes", celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum(),
+  }),
+  headers: Joi.object().keys({}),
+  query: Joi.object().keys({}),
+}), dislikeCard);
 
 module.exports = router;
